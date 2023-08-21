4fd2b86b595de73a64ce900f0b8fe2623df88220
Diego Lindarte <dalindartech@gmail.com>
2023-08-21 09:04:51 -0500

    Actualizacion 2

--- functions.js
+++ functions.js
@@ -23,14 +23,14 @@ const crearContacto = (parentNode, contacto, db) => {
     let borrarContacto = document.createElement('span')
     
     nombreContacto.innerHTML = contacto.nombres;
-    apellidoContacto.innerHTML = contacto.apellido;
+    apellidoContacto.innerHTML = contacto.apellidos;
     numeroContacto.innerHTML = contacto.numero;
     ubicacionContacto.innerHTML = contacto.ubicacion;
     ciudadContacto.innerHTML = contacto.ciudad;
     direccionContacto.innerHTML = contacto.direccion;
     borrarContacto.innerHTML = 'person_remove';
 
-    divContacto.classList.add('lista-contactos');
+    divContacto.classList.add('contactos');
     borrarContacto.classList.add('material-symbols-outlined', 'icon');
 
     borrarContacto.onclick = () => {
